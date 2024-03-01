2d51b3dc88ef5a16cacea6abb2d7db090edb867d
Augusto <correa.augusto9@gmail.com>
2024-02-29 22:31:04 -0300

    aula 29/02

--- aula3.php
+++ aula3.php
@@ -1,13 +1,45 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
 //exibindo a data completa
 echo (date("j/m/y"));
-echo("<br>");
+echo ("<br>");
 
 //dia da semana
 echo (date("D"));
-echo("<br>");
+echo ("<br>");
 echo (date("N"));
 
-echo ("tchais")
+//definindo um array
+//me perdi setando git aqui e nao anotei os codigos, pego depois, ou talvez nao
 
+//outra forma de criar um array
+$numeros = [10, 70, 62, 48, 45, 99, 89];
+
+$posicao_encontrado = array_search(48, $numeros);
+
+if (!$posicao_encontrado) {
+    echo ("<br>Não achemo o numro");
+} else {
+    echo ("<br>O numero escolhido está na posição " . $posicao_encontrado);
+}
+
+//ordenando o array de forma crescente
+
+echo("<br> Array antes de ser ordenado:<br> ");
+print_r($numeros);
+
+sort($numeros);
+echo("<br>Array ordanado de maneira crescente:<br>");
+print_r($numeros);  
+
+rsort($numeros);
+echo("<br>Agora de maneira descrescente com rsort<br>");
+print_r($numeros);
+
+//usando explode para separar uma string em pedaços por meio de um caractere limitador
+$string = "Mateus:20:1.75:Técnico em informática";
+
+$informacao = explode(":", $string);
+
+print_r($informacao);
 ?>
---
+++ exerciciosAula3.php
@@ -0,0 +1,44 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+/*
+
+//exercicio 1
+$hora = date("H");
+
+if($hora >= 6 && $hora < 12) {
+    echo("Bom dia");
+} elseif ($hora >= 12 && $hora < 18) {
+    echo("Boa tarde");
+} else {
+    echo("Boa noite");
+}
+*/
+
+/*
+//exercicio 2
+$dia_mes = date("d");
+$dia_semana = date("N");
+$dias = array("","Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira",
+"Sábado");
+
+$mes = date("n");
+$meses = array("","Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro",
+"Outubro","Novembro","Dezembro");
+
+$ano = date("Y");
+
+echo($dias[$dia_semana] . " $dia_mes de " . "$meses[$mes] de $ano");
+
+*/
+
+// exercicio 3
+ /*
+$vetor = array(1, 5, 8, 9, 2, 3, 7);
+$ehmaior = $vetor[0];
+for($i=0; $i < count($vetor); $i++) {
+    if ($ehmaior < $vetor[$i]) {
+        $ehmaior = $vetor[$i];
+    }
+}
+echo("$ehmaior");
+*/
